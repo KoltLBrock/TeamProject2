9c87258d98441c1dc5992610ee9244ea5dd0d0e5
Scotty <slseethoff1@buffs.wtamu.edu>
2018-04-06 23:29:12 -0500

    Provisional Layout

--- app/src/main/java/com/wtamu/scotty/kolton/cidm4385/ultimatelifeplanner/PlannerActivity.java
+++ app/src/main/java/com/wtamu/scotty/kolton/cidm4385/ultimatelifeplanner/PlannerActivity.java
@@ -1,13 +1,27 @@
 package com.wtamu.scotty.kolton.cidm4385.ultimatelifeplanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PlannerActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button btn;
+    private Button btn1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+
+        Intent incoming = getIntent();
+
     }
 }
